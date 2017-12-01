2a52e9c427457cdf69846450d279e374363993e8
idk <problemsolver@openmailbox.org>
2017-11-30 21:02:28 -0500

    changed username in daemon Dockerfile

--- config.mk
+++ config.mk
@@ -2,7 +2,6 @@
 export username ?= "monero"
 
 export daemon_host = 192.168.1.98
-#export daemon_host ?= 172.17.0.2
 export daemon_port = 18081
 
 export daemon_addr = http://$(daemon_host):$(daemon_port)
