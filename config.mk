886f1cdb2b702b6c5322e0341a2d6ca1ec158ffd
idk <problemsolver@openmailbox.org>
2018-05-30 18:21:47 -0400

    make updates easier in the future.

--- config.mk
+++ config.mk
@@ -8,7 +8,6 @@ export daemon_addr = http://$(daemon_host):$(daemon_port)
 
 export password = $(shell cat password)
 export iface=gui
-export cli_hash=928ad08ff0dea2790c7777a70e610b2d33c35a5df5900fbb050cc8c659237636
 export cli_hash=$(shell curl https://getmonero.org/downloads/ | grep -C 3 linux64 | grep hash | grep CLI |sed 's|<p><strong>SHA256 Hash (CLI):</strong></p> <p class="hash">||g' | tr -d ' ')
 export gui_hash=$(shell curl https://getmonero.org/downloads/ | grep -C 3 linux64 | grep hash | grep GUI |sed 's|<p><strong>SHA256 Hash (GUI):</strong></p> <p class="hash">||g' | tr -d ' ')
 export hash=$(gui_hash)
