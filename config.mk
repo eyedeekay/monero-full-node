35c1c0f6afc9ea3823e123744c4f64b96da56f91
idk <problemsolver@openmailbox.org>
2018-05-30 19:50:28 -0400

    networking stuff, wallet stuff

--- config.mk
+++ config.mk
@@ -8,8 +8,8 @@ export daemon_addr = http://$(daemon_host):$(daemon_port)
 
 export password = $(shell cat password)
 export iface=gui
-export cli_hash=$(shell curl https://getmonero.org/downloads/ 2>/dev/null | grep -C 3 linux64 | grep hash | grep CLI |sed 's|<p><strong>SHA256 Hash (CLI):</strong></p> <p class="hash">||g' | tr -d ' ' 2>/dev/null)
-export gui_hash=$(shell curl https://getmonero.org/downloads/ 2>/dev/null | grep -C 3 linux64 | grep hash | grep GUI |sed 's|<p><strong>SHA256 Hash (GUI):</strong></p> <p class="hash">||g' | tr -d ' ' 2>/dev/null)
+export cli_hash=$(shell curl https://getmonero.org/downloads/ 2>/dev/null | grep -C 3 linux64 | grep hash | grep CLI |sed 's|<p><strong>SHA256 Hash (CLI):</strong></p> <p class="hash">||g' | sed 's|</p>||g' | tr -d ' ' 2>/dev/null)
+export gui_hash=$(shell curl https://getmonero.org/downloads/ 2>/dev/null | grep -C 3 linux64 | grep hash | grep GUI |sed 's|<p><strong>SHA256 Hash (GUI):</strong></p> <p class="hash">||g' | sed 's|</p>||g' | tr -d ' ' 2>/dev/null)
 export hash=$(gui_hash)
 
 DISPLAY = :0
