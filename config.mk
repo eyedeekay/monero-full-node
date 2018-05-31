b57d53ae34dc21d2ceeeff1fef7f271e52ed0055
idk <problemsolver@openmailbox.org>
2018-05-30 21:36:08 -0400

    networking stuff, wallet stuff

--- config.mk
+++ config.mk
@@ -10,7 +10,7 @@ export password = $(shell cat password)
 export iface=cli
 export cli_hash=$(shell curl https://getmonero.org/downloads/ 2>/dev/null | grep -C 3 linux64 | grep hash | grep CLI |sed 's|<p><strong>SHA256 Hash (CLI):</strong></p> <p class="hash">||g' | sed 's|</p>||g' | tr -d ' ' 2>/dev/null)
 export gui_hash=$(shell curl https://getmonero.org/downloads/ 2>/dev/null | grep -C 3 linux64 | grep hash | grep GUI |sed 's|<p><strong>SHA256 Hash (GUI):</strong></p> <p class="hash">||g' | sed 's|</p>||g' | tr -d ' ' 2>/dev/null)
-export hash=$(gui_hash)
+export hash=$(cli_hash)
 
 DISPLAY = :0
 
